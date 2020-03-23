d412a7297bbb46866ec7309ca4412e5457e4c3db
Luther Karper <luther.karper@live.com>
2020-03-23 16:45:13 -0400

    Updates and refactoring in HTML, JS, and CSS files

--- index.js
+++ index.js
@@ -55,17 +55,22 @@
     let currentQuestion = 1;
     let currentScore = 0;
 
-    // begin quiz
-    function beginQuiz() {
-        $('.quiz-field').on('click', '.start-quiz', event => {
-            loadNextQuestion();
-        });
+    // add event listener to button that starts quiz
+    function handlBeginQuiz() {
+        $('.quiz-field').on('click', '.start-quiz', () => loadNextQuestion());
+    }
+
+    // add event listener to button that submits answers for checking
+    function handleSubmitQuestion() {
         $('.quiz-field').on('submit', event => {
             event.preventDefault();
             loadNextAnswer();
         });
-        $('.quiz-field').on('click', '.next-question', event => {
-            event.preventDefault();
+    }
+
+    // add event listener to button that loads the next questions
+    function handleNextQuestion() { 
+        $('.quiz-field').on('click', '.next-question', () => {
             currentQuestion +=1;
             if (currentQuestion > questions.length) {
                 finalScore();
@@ -73,10 +78,11 @@
                 loadNextQuestion();
             }
         });
-        $('.quiz-field').on('click', '.restart', event => {
-            event.preventDefault();
-            restart();
-        });
+    }
+
+    // add event listener to button that restarts quiz
+    function handleRestart() {
+        $('.quiz-field').on('click', '.restart', () => restart());
     }
 
     // fetch question
@@ -102,34 +108,41 @@
         return nextQuestion.correctAnswer;
     }
 
-    // load next question
-    function loadNextQuestion() {
-        const question = parseQuestion();
+    // load options from question object into array of HTML
+    function loadOptions() {
         const options = parseOptions();
         const optionsHTML = [];
         for (let option of options) {
             optionsHTML.push(`
-                <label for="${option}" class="quiz-option">
-                    <input id="${option}" type="radio" name="option" required>
-                    ${option}
-                </label>
+                <input id="${option}" type="radio" name="option" required>
+                <label for="${option}" class="quiz-option">${option}</label>
+                <br>
             `);
         }
+        return optionsHTML;
+    }
+
+    // load next question
+    function loadNextQuestion() {
+        const question = parseQuestion();
+        const options = loadOptions();
         $('.quiz-field').html(`
-            <section class="question-section">
+            <div class="question-section">
                 <div class="question-text">
                     <section class="score-and-result">
                         <p>Quiz progress: ${currentQuestion}/${questions.length}</p>
                         <p>Current score: ${currentScore}/${questions.length}</p>
                     </section>
-                    <h2>${question}</h2>
                     <form>
-                        ${optionsHTML.join('\n')}
+                        <fieldset>
+                            <legend>${question}</legend>
+                            ${options.join('\n')}
+                        </fieldset>
                         <button type="submit" class="check-answer button">Submit</button>
                     </form>
                 </div>
                 <img src="images/question-image.jpg" alt="Relief of four Roman men" class="question-image">
-            </section>
+            </div>
         `);
     }
 
@@ -150,6 +163,41 @@
         return selectedOption;
     }
 
+    // generate HTML for correct answers
+    function handleCorrectAnswer(question, correctAnswer) {
+        $('.quiz-field').html(`
+                <div class="answer-page">
+                    <div class="answer-text">
+                        <section class="score-and-result">
+                            <p>Quiz progress: ${currentQuestion}/${questions.length}</p>
+                            <p>Current score: ${currentScore}/${questions.length}</p>
+                        </section>
+                        <p class="answer-page-question">Question: "${question}"</p>
+                        <p class="answer-page-response">Correct! The answer is "${correctAnswer}"!</p>
+                    </div>
+                    <img src="images/${correctAnswer}-small.jpg" alt="Image of ${correctAnswer}" class="answer-image">
+                </div>
+                <button type="submit" class="next-question button">Next</button>
+            `);
+    }
+
+    // generate HTML for incorrect answers
+    function handleIncorrectAnswer(question, selectedOption, correctAnswer) {
+        $('.quiz-field').html(`
+                <div class="answer-page">
+                    <div class="answer-text">
+                        <p>Quiz progress: ${currentQuestion}/${questions.length}</p>
+                        <p>Current score: ${currentScore}/${questions.length}</p>
+                        <p class="answer-page-question>Question: "${question}"</p>
+                        <p class="answer-page-response">Sorry, your response of "${selectedOption}" is not correct.</p>
+                        <p class="answer-page-response">The correct answer is: "${correctAnswer}."</p>
+                    </div>
+                    <img src="images/${correctAnswer}-small.jpg" alt="Image of ${correctAnswer}" class="answer-image">
+                </div>
+                <button type="submit" class="next-question button">Next</button>
+            `);
+    }
+
     // load next answer
     function loadNextAnswer() {
         const question = parseQuestion();
@@ -157,34 +205,7 @@
         const correctAnswer = parseAnswer();
         const result = checkAnswer(correctAnswer);
         updateScore(result);
-        (result) ? 
-            $('.quiz-field').html(`
-                <div class="answer-page">
-                    <div class="answer-text">
-                        <section class="score-and-result">
-                            <p>Quiz progress: ${currentQuestion}/${questions.length}</p>
-                            <p>Current score: ${currentScore}/${questions.length}</p>
-                        </section>
-                        <p>Question: "${question}"</p>
-                        <h2>Correct! The answer is "${selectedOption}"!</h2>
-                    </div>
-                    <img src="images/${correctAnswer}-small.jpg" alt="Image of ${correctAnswer}" class="answer-image">
-                </div>
-                <button type="submit" class="next-question button">Next</button>
-            `)
-            : $('.quiz-field').html(`
-                <div class="answer-page">
-                    <div class="answer-text">
-                        <p>Quiz progress: ${currentQuestion}/${questions.length}</p>
-                        <p>Current score: ${currentScore}/${questions.length}</p>
-                        <p>Question: "${question}"</p>
-                        <h2>Sorry, your response of "${selectedOption}" is not correct</h2>
-                        <h2>The correct answer is: "${correctAnswer}"</h2>
-                    </div>
-                    <img src="images/${correctAnswer}-small.jpg" alt="Image of ${correctAnswer}" class="answer-image">
-                </div>
-                    <button type="submit" class="next-question button">Next</button>
-            `);
+        result ? handleCorrectAnswer(question, correctAnswer) : handleIncorrectAnswer(question, selectedOption, correctAnswer);
     }
 
     // increase score
@@ -208,7 +229,7 @@
         if (currentScore >= 8) {
             return `
                 <div class="score-text">
-                    <h2>You scored ${currentScore} out of ${questions.length}!</h2>
+                    <p class="final-score-report">You scored ${currentScore} out of ${questions.length}!</p>
                     <p>Tacitus would be proud!</p>
                 </div>
                 <img src="images/Tacitus.jpg" alt="Engraving of the historian Tacitus" class="score-image"> 
@@ -216,7 +237,7 @@
         } else if (currentScore > 5) {
             return `
                 <div class="score-text">
-                    <h2>You scored ${currentScore} out of ${questions.length}!</h2>
+                    <p class="final-score-report">You scored ${currentScore} out of ${questions.length}!</p>
                     <p>An effort worthy of Suetonius!</p>
                 </div>
                 <img src="images/Suetonius.jfif" alt="Bust of the biographer Suetonius" class="score-image">
@@ -224,7 +245,7 @@
         } else {
             return `
                 <div class="score-text">
-                    <h2>You scored ${currentScore} out of ${questions.length}!</h2>
+                    <p class="final-score-report">You scored ${currentScore} out of ${questions.length}!</p>
                     <p>Perhaps you've been reading too much Livy and dwelling on the Republic...</p>
                 </div>
                 <img src="images/Livy.png" alt="Engraving of the historian Livy" class="score-image">
@@ -241,7 +262,10 @@
 
     // initial callbackfunction
     function handleQuizApp () {
-        beginQuiz();
+        handlBeginQuiz();
+        handleNextQuestion();
+        handleSubmitQuestion();
+        handleRestart();
     }
 
     $(handleQuizApp);})();
